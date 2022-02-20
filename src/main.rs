c69a821a35c8944991de5316f394f6592f1928b4
Noriyuki <norifxsc@gmail.com>
2022-02-20 20:58:47 +0900

    add btn

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,23 @@ impl Component for Model {
         // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         let link = ctx.link();
         html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-                <p class="text-green-700">{"test"}</p>
+            <div class="whole ">
+                <div class="direction-area justify-center">
+                    <header class="header py-2 md:py-4 border-b-2 mb-4">
+                        <h1 class="text-2xl font-bold">{"VRC-OSC-ControlPanel"}</h1>
+                    </header>
+                    <div class="btn-wrapper h-80 grid grid-cols-3 grid-rows-3 flex justify-center max-w-screen-md ">
+                        <div class="p-1 justify-self-center"></div>
+                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"STRAIGHT"}</button>
+                        <div class="p-1 justify-self-center"></div>
+                            <button class=" px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"LEFT"}</button>
+                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"JUMP"}</button>
+                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"RIGHT"}</button>
+                        <div class="p-1 justify-self-center"></div>
+                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"BACK"}</button>
+                        <div class="p-1 justify-self-center"></div>
+                    </div>
+                </div>
             </div>
         }
     }
