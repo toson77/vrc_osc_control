0a19c44812eb7abd57ed9fbf6a8ad37670a882be
Noriyuki <norifxsc@gmail.com>
2022-03-01 16:25:07 +0900

    success websocket

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
 enum Msg {
-    ButtonClick,
+    ButtonDown,
+    ButtonUp,
 }
 
 struct OperationStatus {
@@ -20,20 +21,52 @@ struct Model {
     ws: Result<WebSocket, Error>,
 }
 
-impl Component for OperationComponent {
+impl Component for OperationStatus {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
+        Self {
+            isUp: false,
+            isDown: false,
+            isRight: false,
+            isLeft: false,
+            isJump: false,
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
+            Msg::ButtonDown => {
+                self.isUp = true;
                 // the value has changed so we need to
                 // re-render for it to appear on the page
+                log::info!("isUp:true");
+                //websocket
+                let mut ws = WebSocket::open("ws://localhost:8000").unwrap();
+                let (mut write, mut read) = ws.split();
+                spawn_local(async move {
+                    write
+                        .send(Message::Text(String::from("test")))
+                        .await
+                        .unwrap();
+                    write
+                        .send(Message::Text(String::from("test 2")))
+                        .await
+                        .unwrap();
+                });
+                spawn_local(async move {
+                    while let Some(msg) = read.next().await {
+                        log::info!("{}", format!("1. {:?}", msg));
+                    }
+                    log::info!("WebSocket closed");
+                });
+
+                true
+            }
+            Msg::ButtonUp => {
+                self.isUp = false;
+                log::info!("isUp:false");
                 true
             }
         }
@@ -50,7 +83,7 @@ impl Component for OperationComponent {
                     </header>
                     <div class="btn-wrapper h-80 grid grid-cols-3 grid-rows-3 flex justify-center max-w-screen-md ">
                         <div class="p-1 justify-self-center"></div>
-                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"STRAIGHT"}</button>
+                            <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500" onmousedown={link.callback(|_| Msg::ButtonDown)} onmouseup={link.callback(|_| Msg::ButtonUp)}>{"STRAIGHT"}</button>
                         <div class="p-1 justify-self-center"></div>
                             <button class=" px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"LEFT"}</button>
                             <button class="px-2 py-1 m-1 bg-blue-400 text-white sm:text-lg font-semibold rounded hover:bg-blue-500">{"JUMP"}</button>
@@ -66,5 +99,6 @@ impl Component for OperationComponent {
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<OperationStatus>();
 }
